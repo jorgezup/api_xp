977d1e74cd7129a3de8564da77c3677d0f496cbf
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-17 17:09:40 -0400

    refactor: change path to entities and migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -11,6 +11,6 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: ["/src/models/entities/*.ts"],
-    migrations: ["/src/database/migrations/*.ts"],
+    entities: [`${__dirname}/**/entities/*.{ts,js}`],
+    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 })
