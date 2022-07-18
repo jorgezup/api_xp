9466b9291b430894d2e6356a03863c63b76088f6
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-18 13:48:10 -0400

    feat: transaction interface

---
+++ src/interfaces/transaction.interface.ts
@@ -0,0 +1,10 @@
+export enum TypeTransaction {
+    DEPOSIT = 'deposit',
+    WITHDRAW = 'withdraw'
+}
+
+export interface IAccountTransaction {
+    value: number;
+    type: TypeTransaction;
+    codClient: number;
+}
