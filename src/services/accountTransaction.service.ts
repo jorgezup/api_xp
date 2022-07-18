1f2c441b36a7ec8aed60a84e6ca2efb900bd45d4
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-18 14:05:14 -0400

    lint: solve lint error in services

--- src/services/accountTransaction.service.ts
+++ src/services/accountTransaction.service.ts
@@ -4,56 +4,56 @@ import { IAccountTransaction } from "../interfaces/transaction.interface";
 import { AccountService } from "./account.service";
 
 export class AccountTransactionService {
-    async depositTransaction(transaction: any) {
-        const clientAccount = await accountRepository.find({
-            where: {
-                client: {codClient: transaction.codClient}
-            },
-            relations: ['client']
-        })
+  async depositTransaction(transaction: IAccountTransaction) {
+    const clientAccount = await accountRepository.find({
+      where: {
+        client: { codClient: transaction.codClient },
+      },
+      relations: ["client"],
+    });
 
-        if (clientAccount.length === 0) {
-            return new Error('Client does not exists')
-        }
-        const newTransaction = accountTransactionRepository.create({
-            type: transaction.type,
-            value: transaction.value,
-            account: clientAccount[0]
-        })
-
-        await accountTransactionRepository.save(newTransaction)
-
-        return newTransaction        
+    if (clientAccount.length === 0) {
+      return new Error("Client does not exists");
     }
-    async withdrawTransaction(transaction: IAccountTransaction) {
-        console.log(transaction)
-        const clientAccount = await accountRepository.find({
-            where: {
-                client: {codClient: transaction.codClient}
-            },
-            relations: ['client']
-        })
+    const newTransaction = accountTransactionRepository.create({
+      type: transaction.type,
+      value: transaction.value,
+      account: clientAccount[0],
+    });
 
-        if (clientAccount.length === 0) {
-            return new Error('Client does not exists')
-        }
+    await accountTransactionRepository.save(newTransaction);
 
-        const accountService = new AccountService()
+    return newTransaction;
+  }
+  async withdrawTransaction(transaction: IAccountTransaction) {
+    console.log(transaction);
+    const clientAccount = await accountRepository.find({
+      where: {
+        client: { codClient: transaction.codClient },
+      },
+      relations: ["client"],
+    });
 
-        const {saldo} = await accountService.balance(transaction.codClient)
-
-        if (saldo - transaction.value < 0) {
-            return new Error ("There's not enought money")
-        }
-
-        const newTransaction = accountTransactionRepository.create({
-            type: transaction.type,
-            value: - transaction.value,
-            account: clientAccount[0]
-        })
-
-        await accountTransactionRepository.save(newTransaction)
-
-        return newTransaction   
+    if (clientAccount.length === 0) {
+      return new Error("Client does not exists");
     }
+
+    const accountService = new AccountService();
+
+    const { saldo } = await accountService.balance(transaction.codClient);
+
+    if (saldo - transaction.value < 0) {
+      return new Error("There's not enought money");
+    }
+
+    const newTransaction = accountTransactionRepository.create({
+      type: transaction.type,
+      value: -transaction.value,
+      account: clientAccount[0],
+    });
+
+    await accountTransactionRepository.save(newTransaction);
+
+    return newTransaction;
+  }
 }
--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,24 +1,24 @@
 import { Client } from "../database/models/entities/Client";
-import { IClient } from "../interfaces/client.interface";
-import { clientRepository } from "../database/models/repositories/client.repository";
 import { accountRepository } from "../database/models/repositories/account.repository";
+import { clientRepository } from "../database/models/repositories/client.repository";
+import { IClient } from "../interfaces/client.interface";
 
 export class ClientService {
-    async createClient(client: IClient): Promise<Client | Error> {
-        if (await clientRepository.findOneBy({email: client.email})) {
-            return new Error('Email already exists')
-        }
-
-        const newClient = clientRepository.create({
-            ...client,
-            // codClient: Math.floor(100000 + Math.random() * 900000)
-        })
-
-        const newAccount = accountRepository.create({client: newClient})
-
-        await clientRepository.save(newClient)
-        await accountRepository.save(newAccount)
-
-        return newClient
+  async createClient(client: IClient): Promise<Client | Error> {
+    if (await clientRepository.findOneBy({ email: client.email })) {
+      return new Error("Email already exists");
     }
+
+    const newClient = clientRepository.create({
+      ...client,
+      // codClient: Math.floor(100000 + Math.random() * 900000)
+    });
+
+    const newAccount = accountRepository.create({ client: newClient });
+
+    await clientRepository.save(newClient);
+    await accountRepository.save(newAccount);
+
+    return newClient;
+  }
 }
