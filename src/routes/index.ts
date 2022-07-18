0a0d1dabd33d4004617f649257829c6737bc7a26
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-18 13:50:57 -0400

    feat: added route conta

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express"
+import { AccountRoutes } from "./account.route"
 import { ClientRoutes } from "./clients.route"
 
 const routes = express.Router()
 
 routes.use('/clients', ClientRoutes)
+routes.use('/conta', AccountRoutes)
 
 export {routes}
