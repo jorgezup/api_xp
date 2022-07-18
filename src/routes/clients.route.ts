32a4d2f4c2c1bb0e84c61ca84a47b60ca5e33302
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-17 22:39:35 -0400

    feat: client route and added route post

---
+++ src/routes/clients.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { ClientController } from "../controllers/clients.controller";
+
+const routes = Router()
+
+routes.post('/', new ClientController().create)
+
+export {routes as ClientRoutes}
