aefce6bac501f4a0e8f520ec3020a76ecc1e3051
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-17 22:53:01 -0400

    feat: added fucntion to create account when create a client

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,6 +1,7 @@
 import { Client } from "../database/models/entities/Client";
 import { IClient } from "../interfaces/client.interface";
 import { clientRepository } from "../database/models/repositories/client.repository";
+import { accountRepository } from "../database/models/repositories/account.repository";
 
 export class ClientService {
     async createClient(client: IClient): Promise<Client | Error> {
@@ -13,7 +14,10 @@ export class ClientService {
             // codClient: Math.floor(100000 + Math.random() * 900000)
         })
 
+        const newAccount = accountRepository.create({client: newClient})
+
         await clientRepository.save(newClient)
+        await accountRepository.save(newAccount)
 
         return newClient
     }
