0ef1554ec56b14a9b568b210b84fa8a83a5e32a1
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-17 22:40:13 -0400

    feat: added routes into express app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { AppDataSource } from "./database/data-source";
+import { routes } from "./routes";
 
 const PORT = process.env.PORT
 
@@ -7,6 +8,8 @@ AppDataSource.initialize().then(() => {
     const app = express()
 
     app.use(express.json())
+
+    app.use(routes)
 
     app.listen(PORT, () => {console.log(`Server is running on ${PORT}`)})
 }).catch((error) => console.log("TypeORM initialization error: ", error))
