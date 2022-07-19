38ff99d36c44d469c2dcac69726f7c3e5f5ae310
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-18 21:57:26 -0400

    feat: added validate client into client routes

--- src/routes/clients.route.ts
+++ src/routes/clients.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { ClientController } from "../controllers/clients.controller";
+import { clientMiddleware } from "../middlewares/clients.middleware";
 
 const routes = Router();
 
-routes.post("/", new ClientController().create);
+routes.post("/", clientMiddleware, new ClientController().create);
 
-export { routes as ClientRoutes };
+export { routes as clientRoutes };
