dda376e72df9be1a8eff07ff51929ce1d7b56814
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-18 21:57:43 -0400

    feat: account transaction middleware

---
+++ src/middlewares/accountTransaction.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import joi from "joi";
+
+const schema = joi.object({
+  value: joi.number().positive().required(),
+});
+
+const validateTransaction = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    const { type, message } = error.details[0];
+    next({
+      statusCode: type === "any.required" ? 400 : 422,
+      messageType: message,
+    });
+  }
+
+  next();
+};
+
+export { validateTransaction as accountTransactionMiddleware };
