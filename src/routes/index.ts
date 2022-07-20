5a25fc2562f1465d28521fc1a57157ce4d3223ac
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-20 17:25:34 -0400

    refactor: endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,14 @@ import express from "express";
 
 import { accountRoutes } from "./account.route";
 import { clientRoutes } from "./clients.route";
+import { investimentsRoutes } from "./investiments.route";
+import { stocksRoute } from "./stocks.route";
 
 const routes = express.Router();
 
-routes.use("/clients", clientRoutes);
+routes.use("/clientes", clientRoutes);
 routes.use("/conta", accountRoutes);
+routes.use("/investimentos", investimentsRoutes);
+routes.use("/acoes", stocksRoute);
 
 export { routes };
