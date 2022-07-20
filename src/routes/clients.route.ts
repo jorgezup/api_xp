aeea49499ef7d1d340afb52fb86b5c05c0228fbc
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-20 17:23:57 -0400

    refactor: routes endpoints

--- src/routes/clients.route.ts
+++ src/routes/clients.route.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import { ClientController } from "../controllers/clients.controller";
 import { clientMiddleware } from "../middlewares/clients.middleware";
+import { loginMiddleware } from "../middlewares/login.middleware";
 
 const routes = Router();
 
-routes.post("/", clientMiddleware, new ClientController().create);
-routes.post("/login", new ClientController().login);
+routes.post("/criar", clientMiddleware, new ClientController().create);
+routes.post("/entrar", loginMiddleware, new ClientController().login);
 
 export { routes as clientRoutes };
