9b42271427681ac1081ccfab9081cc17fb1ad207
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-21 13:45:10 -0400

    fix: added ssl data-source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -13,4 +13,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
   entities: [`${__dirname}/**/entities/*.{ts,js}`],
   migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
