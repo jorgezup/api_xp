a2cc5e0f26e5b09f86d70f408ef80c026e461569
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-20 23:38:38 -0400

    feat: added update for stock

--- src/controllers/stocks.controller.ts
+++ src/controllers/stocks.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 import { StocksService } from "../services/stocks.service";
+import HttpException from "../shared/http.exception";
 
 export class StocksController {
   async create(req: Request, res: Response) {
@@ -20,6 +21,20 @@ export class StocksController {
     const service = new StocksService();
 
     const response = await service.listStocks();
+
+    return res.status(200).json(response);
+  }
+  async update(req: Request, res: Response) {
+    const { stock } = req.params;
+    const service = new StocksService();
+
+    const response = await service.updateStock(stock);
+
+    if (response instanceof HttpException) {
+      return res
+        .status(response.statusCode)
+        .json({ message: response.messageType });
+    }
 
     return res.status(200).json(response);
   }
