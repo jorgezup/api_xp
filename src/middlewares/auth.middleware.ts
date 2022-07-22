440fdba18972d290766832225c7a206ec9cc6f76
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-22 15:58:19 -0400

    refactor: added validation to work with swagger

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -7,8 +7,28 @@ export const authenticationMiddleware = async (
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  const token = req.headers.authorization;
+  // const token = req.headers.authorization;
+  if (!req.headers.authorization) {
+    return next({
+      messageType: "Unauthorized",
+      statusCode: 401,
+    });
+  }
+  let token: string | undefined;
+  const [Bearer, tokenString] = req.headers.authorization.split(" ");
+  /* Insomnia não passa a palavra Bearer, porém o Swagger utiliza a palavra Bearer antes do token 
+  essa lógica, o authorization é validado, e feito um split, caso haja a palavra Bearer, o tokenString
+  é atribuído ao token, caso contrário o Bearer é atribuído ao token
+  */
+
+  if (Bearer === "Bearer") {
+    token = tokenString;
+  } else {
+    token = Bearer;
+  }
+
   const payload = await authenticateToken(token);
+
   if (!payload || payload instanceof Error) {
     return next({
       messageType: "Unauthorized",
