7a2fdc7349ae8207a9ff70f45ad2b5add86776a6
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-22 11:28:20 -0400

    refactor: removed put route and added endpoit to post

--- src/routes/stocks.route.ts
+++ src/routes/stocks.route.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 import { StocksController } from "../controllers/stocks.controller";
+import { authenticationMiddleware } from "../middlewares/auth.middleware";
 
 const routes = Router();
 
-// routes.use(authenticationMiddleware);
+routes.use(authenticationMiddleware);
 
 routes.get("/", new StocksController().list);
 /* Is Admin */
-routes.post("/", new StocksController().create);
-routes.put("/:stock", new StocksController().update);
+routes.post("/cadastrar", new StocksController().create);
 
 export { routes as stocksRoute };
