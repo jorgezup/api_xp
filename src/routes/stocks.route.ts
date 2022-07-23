6158d64ee8344f280231a9c880f16a13fb69f675
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-23 15:03:19 -0400

    feat: added validade is admin into route

--- src/routes/stocks.route.ts
+++ src/routes/stocks.route.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 
 import { StocksController } from "../controllers/stocks.controller";
 import { authenticationMiddleware } from "../middlewares/auth.middleware";
+import { validateIsAdmin } from "../middlewares/isAdmin";
 
 const routes = Router();
 
 routes.use(authenticationMiddleware);
 
 routes.get("/", new StocksController().list);
-/* Is Admin */
-routes.post("/cadastrar", new StocksController().create);
+routes.post("/cadastrar", validateIsAdmin, new StocksController().create);
 
 export { routes as stocksRoute };
